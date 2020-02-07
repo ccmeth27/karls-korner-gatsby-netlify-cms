81e793fa3418ca49b96014652e2ea24d71c553b6
Connor Meth <connormeth27@gmail.com>
2020-02-07 15:13:49 -0500

    hiding about page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -63,9 +63,9 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>
-              <Link className="navbar-item" to="/about">
+              {/* <Link className="navbar-item" to="/about">
                 About
-              </Link>
+              </Link> */}
               <Link className="navbar-item" to="/contact">
                 Contact
               </Link>
